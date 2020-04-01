e8120f84046a09026766fec74c6e314f4dafe7fa
erkins1 <35502998+erkins1@users.noreply.github.com>
2020-03-31 20:08:25 -0600

    Added some Models

---
+++ SacramentPlanner/Models/Agenda.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SacramentPlanner.Models
+{
+    public class Agenda
+    {
+        public int AgendaID { get; set; }
+        public Category Section { get; set; }
+        public int Member { get; set; }
+        public int Hymn { get; set; }
+        public string Special_Event_Text { get; set; }
+        public string Notes { get; set; }
+        public string Subject { get; set; }
+
+        [ForeignKey("Member")]
+        public Directory Directory { get; set; }
+
+        [ForeignKey("Hymn")]
+        public Hymn Hymn { get; set; }
+
+
+        public enum Category
+        {
+            Speaker,
+            Special_Event,
+            Hymn,
+            Special_Musical_Number
+        }
+    }
+}
---
+++ SacramentPlanner/Models/Meeting.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SacramentPlanner.Models
+{
+    public class Meeting
+    {
+        public int MeetingID { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        public int WardId { get; set; }
+        public int Presiding { get; set; }
+        public int Conducting { get; set; }
+        public int Opening_Hymn { get; set; }
+        public int Invocation { get; set; }
+        public bool Ward_Business { get; set; }
+        public bool Sacrament { get; set; }
+        public int Sacrament_Hymn { get; set; }
+        public int Closing_Hymn { get; set; }
+        public int Benediction { get; set; }
+
+
+        [Required, ForeignKey("WardID")]
+        public Ward Ward { get; set; }
+
+        [Required, ForeignKey("Presiding")]
+        public Directory Directory { get; set; }
+
+        [Required, ForeignKey("Conducting")]
+        public Directory Directory { get; set; }
+
+        [Required, ForeignKey("Opening_Hymn")]
+        public Hymn Hymn { get; set; }
+
+        [ForeignKey("Invocation")]
+        public Directory Directory { get; set; }
+
+        [ForeignKey("Sacrament_Hymn")]
+        public Hymn Hymn { get; set; }
+
+        [Required, ForeignKey("Closing_Hymn")]
+        public Hymn Hymn { get; set; }
+
+        [ForeignKey("Benediction")]
+        public Directory Directory { get; set; }
+
+
+
+    }
+}
