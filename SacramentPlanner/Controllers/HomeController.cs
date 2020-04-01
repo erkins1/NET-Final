0fe3a5126f1f318118752064fe8f09bad401cf8c
Bhogge2 <35310873+Bhogge2@users.noreply.github.com>
2020-04-01 15:40:48 -0600

    added links to nav

--- SacramentPlanner/Controllers/HomeController.cs
+++ SacramentPlanner/Controllers/HomeController.cs
@@ -23,10 +23,16 @@ namespace SacramentPlanner.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Directory()
         {
             return View();
         }
+
+        
+        //public IActionResult ()
+        //{
+        //    return View();
+        //}
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
