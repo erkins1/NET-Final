70c1d8d84aa44a7e27aa984cdacb975f95c7621a
erkins1 <35502998+erkins1@users.noreply.github.com>
2020-04-02 14:51:57 -0600

    Notes

--- SacramentPlanner/wwwroot/js/agendaItems.js
+++ SacramentPlanner/wwwroot/js/agendaItems.js
@@ -1,1 +1,23 @@
-﻿
+﻿//This Javascript code adds in indiviudal agenda items as needed
+
+
+//This creates the dropdown that helps the user choose the item they want to add
+function addNewAgendaItem() {
+
+}
+
+
+//An event listener that controls what the user will be able to input
+//  runs once the user selects from the dropdown list
+//  e.g. Add a hymn or speaker etc.
+function buildAgendaItem() {
+
+}
+
+
+//This might be some processing to help write the agenda items to the database
+
+
+
+
+
